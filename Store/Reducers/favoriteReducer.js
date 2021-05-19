5fb5dcca36cc649f6b91bf8cc95ad0ed0cf5f614
Issa Coulibaly <stagiaire_digital@mansabank.com>
2021-05-19 14:53:34 +0000

    track files and reacte favoriteFilm reducer

---
+++ Store/Reducers/favoriteReducer.js
@@ -0,0 +1,25 @@
+const initialState = { favoritesFilm: []}
+
+function toogleFavorite(state = initialState, action) {
+	let nextState
+	switch (action.type) {
+		case 'TOOGLE_FAVORITE':
+		    const favoriteFilmIndex = state.favoritesFilm.findIndex(item => item.id === action.value.id)
+			if (favoriteFilmIndex != -1) {
+				nextState = {
+					...state,
+					favoritesFilm: state.favoritesFilm.filter((item,index) => index !== favoriteFilmIndex)
+				}
+			} else {
+				nextState = {
+					...state,
+					favoritesFilm: [...state.favoritesFilm, action.value]
+				}
+			}
+			return nextState || state;
+	default:
+		return state;
+	}
+}
+
+export default toogleFavorite;
